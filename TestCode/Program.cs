20c361fe13ef91f7eb892aec666ac48222373544
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-04-03 17:50:08 -0400

    spacing

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -8,17 +8,15 @@ namespace TestCode
             if (a == 1 || a == 0){
                 return 1;
             }
-
             return fibrecurs(a - 1) + fibrecurs(a - 2);
         }
-
         static int[] array = new int[4000];
-
 
         static int FibTopDown(int n){
             if (n == 1 || n == 0){
                 return 1;
             }
+
             if (array[n] != 0){
                 return array[n];
             }
@@ -27,6 +25,7 @@ namespace TestCode
                 return array[n];
             }
         }
+
         static int fibbottom(int n){
             int[] array = new int[n + 1];
             array[0] = 1;
@@ -38,29 +37,26 @@ namespace TestCode
             return array[n];
         }
 
-        static double binet( int n){
+        static double binet(int n){
             ++n;
-          double phi = (1 + Math.Sqrt(5))*0.5;
-          double fib = (Math.Pow(phi,n) - Math.Pow(1-phi,n))/Math.Sqrt(5);
+            double phi = (1 + Math.Sqrt(5)) * 0.5;
+            double fib = (Math.Pow(phi, n) - Math.Pow(1 - phi, n)) / Math.Sqrt(5);
             return Math.Round(fib);
-    }
-
+        }
         public static void Main(string[] args){
             var x = 400;
-
             var mytime = DateTime.Now;
             /*Console.WriteLine("recursive:{0}",fibrecurs(x));
             Console.WriteLine(DateTime.Now-mytime);*/
             mytime = DateTime.Now;
-            Console.WriteLine("topdown:{0}",FibTopDown(x));
-            Console.WriteLine(DateTime.Now-mytime);
+            Console.WriteLine("topdown:{0}", FibTopDown(x));
+            Console.WriteLine(DateTime.Now - mytime);
             mytime = DateTime.Now;
-            Console.WriteLine("bottomup{0}",fibbottom(x));
-            Console.WriteLine(DateTime.Now-mytime);
+            Console.WriteLine("bottomup{0}", fibbottom(x));
+            Console.WriteLine(DateTime.Now - mytime);
             mytime = DateTime.Now;
-            Console.WriteLine("bonets: {0}",binet(x));
-            Console.WriteLine(DateTime.Now-mytime);
-            
+            Console.WriteLine("binets: {0}", binet(x));
+            Console.WriteLine(DateTime.Now - mytime);
         }
     }
 }
